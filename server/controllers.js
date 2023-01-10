f7a4e80a8bfe76f3f916c002ed6168bdf3b6f716
Ivan Day <ivanday@utexas.edu>
2023-01-10 10:26:05 -0600

    Add first request to reddit api

---
+++ server/controllers.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+axios.get('http://www.reddit.com/search.json?q=https://www.commondreams.org/news/congressional-budget-office&sort=new&limit=5')
+  .then((response) => {
+    for (const post of response.data.data.children) {
+      console.log(post.data.permalink);
+    }
+
+    //console.log(response.data.data.children);
+  })
