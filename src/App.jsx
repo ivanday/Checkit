548195017b4f6a3e574ed5226845f888aadfdbe2
Ivan Day <ivanday@utexas.edu>
2023-01-10 14:20:41 -0600

    Updated packages and App.jsx file to test local server endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 /*global chrome*/
 function App() {
+  let currentUrl = '';
+  let {redditLink, setRedditLinks} = useState([]);
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    currentUrl = tabs[0].url;
+  });
+  axios.get('http://localhost:4000/reddit_posts/', {url: 'https://www.commondreams.org/news/congressional-budget-office'})
+  .then((response) => {
+    console.log(response)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+  setTimeout(() => {console.log(currentUrl)}, 5000);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        <div>
-          {chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-            let url = tabs[0].url;
-            console.log(url);
-          })}
-        </div>
-        </a>
-      </header>
+      <h1>Posts at your url: </h1>
+      <div>
+        {
+
+        }
+      </div>
     </div>
   );
 }
