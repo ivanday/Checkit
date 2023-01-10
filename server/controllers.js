c5c28d04e4b74ec31ab879e1a2ff57604a18c2b2
Ivan Day <ivanday@utexas.edu>
2023-01-10 11:05:17 -0600

    add body parsers, troubleshoot request issues

--- server/controllers.js
+++ server/controllers.js
@@ -1,10 +1,14 @@
 const axios = require('axios');
 
-axios.get('http://www.reddit.com/search.json?q=https://www.commondreams.org/news/congressional-budget-office&sort=new&limit=5')
-  .then((response) => {
-    for (const post of response.data.data.children) {
-      console.log(post.data.permalink);
-    }
+// axios.get('http://www.reddit.com/search.json?q=https://www.commondreams.org/news/congressional-budget-office&sort=new&limit=5')
+//   .then((response) => {
+//     for (const post of response.data.data.children) {
+//       console.log(post.data.permalink);
+//     }
 
-    //console.log(response.data.data.children);
-  })
+//     //console.log(response.data.data.children);
+//   })
+
+exports.getRedditPosts = (req, res) => {
+  console.log(req);
+};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
+const path = require('path');
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const controllers = require('./controllers.js');
 
-const port = 3000;
+const port = 4000;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/reddit_posts', (req, res) => {
+  console.log(req);
+  res.status(200).end();
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
