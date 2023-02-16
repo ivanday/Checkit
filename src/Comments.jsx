71706a23f190c7295cee977f6adb0ef815356d53
Ivan Day <ivanday@utexas.edu>
2023-02-16 16:03:32 -0600

    changed comment api requests from server side to client side

--- src/Comments.jsx
+++ src/Comments.jsx
@@ -7,19 +7,27 @@ export default function Comments({post}) {
   useEffect(() => {
     document.getElementById(post.id).addEventListener("click", () => {
       if (comments.length === 0) {
-        axios.get('http://localhost:4000/reddit_comments', {
-          params: {
-            id: post.id,
-            subreddit: post.subreddit
-          }
-        })
+        axios.get(`https://www.reddit.com/r/${post.subreddit}/comments/${post.id}.json?`)
         .then((response) => {
-          //console.log('server response: ', response);
-          setComments(response.data);
+          // console.log(response.data[1].data.children);
+          setComments(response.data[1].data.children);
         })
         .catch((err) => {
           console.log(err);
         })
+        // axios.get('http://localhost:4000/reddit_comments', {
+        //   params: {
+        //     id: post.id,
+        //     subreddit: post.subreddit
+        //   }
+        // })
+        // .then((response) => {
+        //   //console.log('server response: ', response);
+        //   setComments(response.data);
+        // })
+        // .catch((err) => {
+        //   console.log(err);
+        // })
       }
       setShowComments(!showComments);
     })
