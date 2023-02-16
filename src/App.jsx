5402d274333c06366fd88654cdaa54f9db8e70f2
Ivan Day <ivanday@utexas.edu>
2023-02-16 16:00:34 -0600

    change api requests to be done client-side instead of server side

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,39 @@ function App() {
   useEffect(()=> {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
       console.log('url:', tabs[0].url);
-      axios.get('http://localhost:4000/reddit_posts/', {
-        params: {
-          url: tabs[0].url
-        }
-      })
+      let permalinks = [];
+      axios.get(`http://www.reddit.com/search.json?q=${tabs[0].url}&sort=relevant&limit=5`)
       .then((response) => {
-        console.log(response)
-        setRedditLinks(response.data);
+        //console.log(response.data.data.children);
+        if (response.data.data.children) {
+          console.log(response.data.data.children[0]);
+          for (const post of response.data.data.children) {
+            permalinks.push({
+              permalink: post.data.permalink,
+              id: post.data.id,
+              subreddit: post.data.subreddit,
+              title: post.data.title
+            });
+          }
+          setRedditLinks(permalinks);
+        }
       })
       .catch((err) => {
         console.log(err);
       })
+
+      // axios.get('http://localhost:4000/reddit_posts/', {
+      //   params: {
+      //     url: tabs[0].url
+      //   }
+      // })
+      // .then((response) => {
+      //   console.log(response)
+      //   setRedditLinks(response.data);
+      // })
+      // .catch((err) => {
+      //   console.log(err);
+      // })
     });
   }, [])
   return (
